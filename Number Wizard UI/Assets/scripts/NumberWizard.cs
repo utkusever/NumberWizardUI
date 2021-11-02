91c8065e08d5f8d5653454a8c0562504bce4d2c4
utkusever <c.utkusever@outlook.com>
2021-11-02 22:10:26 +0300

    Initial commit

---
+++ Number Wizard UI/Assets/scripts/NumberWizard.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max; // degerler class'a alındı ki update metodu da erişebilsin.
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+
+        calc();
+
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess +1 ;
+        calc();
+    }
+    public void OnPressLower()
+    {
+        max = guess -1;
+        calc();
+    }
+
+    void calc()
+    {
+        guess = Random.Range(min, max); //min inclue max exclue
+        guessText.text = guess.ToString();
+    }
+    
+}
